754e194895790fdf790ebde8845c1b2abb5afc76
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-13 15:40:00 +0300

    Add files via upload
    my_dict = {'Steve': 2004, 'Any': 1989, 'Jon': 1975}
    print(my_dict)
    print(my_dict['Steve'])
    print(my_dict.get('Brogar'))
    my_dict.update({'Vera': 2006,
                    'Sera': 2009})
    del my_dict['Jon']
    print(my_dict)
    my_set = {1, 1, 2, 5, 3, 3, 2, 5, 'No', 'Yes', (7, 3, 4), 'Yes', 'No'}
    print(my_set)
    my_set.add(8), my_set.add(9), my_set.remove(5)
    print(my_set)

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict = {'Steve': 2004, 'Any': 1989, 'Jon': 1975}
+print(my_dict)
+print(my_dict['Steve'])
+print(my_dict.get('Brogar'))
+my_dict.update({'Vera': 2006,
+                'Sera': 2009})
+del my_dict['Jon']
+print(my_dict)
+my_set = {1, 1, 2, 5, 3, 3, 2, 5, 'No', 'Yes', (7, 3, 4), 'Yes', 'No'}
+print(my_set)
+my_set.add(8), my_set.add(9), my_set.remove(5)
+print(my_set)
