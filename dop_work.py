2d214daeb1356f4b6632698066322b8d69165a28
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-16 14:08:26 +0300

    Update dop_work.py

--- dop_work.py
+++ dop_work.py
@@ -1,11 +1,11 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-number_1 =(sum(grades[0]))/len(grades[0])
-number_2 =(sum(grades[1]))/len(grades[1])
-number_3 =(sum(grades[2]))/len(grades[2])
-number_4 =(sum(grades[3]))/len(grades[3])
-number_5 =(sum(grades[4]))/len(grades[4])
+i = 0
+b = []
+for number in grades:
+    b.append(sum(grades[i]) / len(grades[i]))
+    i = i+1
 a = list(students)
 a.sort()
-a = {a[0]: number_1, a[1]: number_2, a[2]: number_3, a[3]: number_4, a[4]: number_5}
+a = {a[0]: b[0], a[1]: b[1], a[2]: b[2], a[3]: b[3], a[4]: b[4]}
 print(a)
