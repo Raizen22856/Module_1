69b2cf0412c4600bb90f8d16ff07094693e3938a
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-16 13:35:27 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -5,6 +5,7 @@ print(my_dict.get('Brogar'))
 my_dict.update({'Vera': 2006,
                 'Sera': 2009})
 del my_dict['Jon']
+print(my_dict.get('Jon'))
 print(my_dict)
 my_set = {1, 1, 2, 5, 3, 3, 2, 5, 'No', 'Yes', (7, 3, 4), 'Yes', 'No'}
 print(my_set)
